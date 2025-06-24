1dde34b5ae5f0ee97d211be6fa4a58f0a754d9aa
Thomas Anto Moothedan <thomas.anto.moothedan@gmail.com>
2025-06-24 09:43:23 +0530

    Add files via upload

---
+++ FactorLevels.R
@@ -0,0 +1,6 @@
+gender <- factor(c("Female","Male","Other"),
+                      levels = c("Male","Female","Other"))
+
+str(gender)
+levels(gender)
+gender
---
+++ FactorsMonths.R
@@ -0,0 +1,5 @@
+months <- factor(c("Dec","Dec","Oct","Mar","Mar","Jul"),
+                 levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"))
+
+levels(months)
+table(months)
